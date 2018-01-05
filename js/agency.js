f1e96fd4c364cc4dd3245c811b4de06d05048bf1
Brendan Asselstine <mail.asselstine@gmail.com>
2018-01-05 11:34:51 -0800

    Updated screenshots and video

--- js/agency.js
+++ js/agency.js
@@ -2,4 +2,13 @@ $(function() {
   $('a.download').on('click', function () {
     ga('send', 'event', 'iOS App Link');
   });
+
+  $('.overlay-container_content_img').load(function () {
+    $(this).animate({ opacity: 1 }, 1000)
+  })
+  $('.overlay-container_content_img').attr('src', 'https://s3.amazonaws.com/replay-landing-page/FirstFrame.jpg?r=' + Math.random())
+
+  $('video.overlay-container_content_media').on('canplay', function () {
+    $(this).delay(1000).animate({ opacity: 1 }, 1000)
+  })
 });
